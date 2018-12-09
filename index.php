13976dae7a5c55bc4d767d0eb2d53cb0408a1e83
Roman Smirnov <r.smirnov86@gmail.com>
2018-12-09 21:27:08 +0600

    добавлен простой список категории

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ $is_auth = (bool) rand(0, 1);
 
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
+
+$list_category = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
